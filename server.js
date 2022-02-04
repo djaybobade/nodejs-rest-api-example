b3dd251143be13b479a2f97fc0c840e25309ef2c
djaybobade <63049815+djaybobade@users.noreply.github.com>
2022-02-04 18:04:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const emailMisspelled = require("email-misspelled");
+const emailChecker = emailMisspelled.default({ domains: emailMisspelled.top100 });
 
 app.set("port", process.env.PORT || 4000);
 
 app.get('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method." }
+   let response = { "response" : "This is GET method." }
    console.log(response);
    res.end(JSON.stringify(response));
 })
 
+
 app.get('/:id', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method with id=" + req.params.id + "." }
+   //var response = { "response" : "This is GET method with id=" + req.params.id + "." }
+   let response = emailChecker(req.params.id)
    console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.post('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is POST method." }
+   let response = { "response" : "This is POST method." }
    console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.put('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is PUT method." }
+   let response = { "response" : "This is PUT method." }
    console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.delete('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is DELETE method." }
+   let response = { "response" : "This is DELETE method." }
    console.log(response);
    res.end(JSON.stringify(response));
 })
 
-var server = app.listen(app.get("port"), function () {
+const server = app.listen(app.get("port"), function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  const host = server.address().address
+  const port = server.address().port
 
   console.log("Node.js API app listening at http://%s:%s", host, port)
 
